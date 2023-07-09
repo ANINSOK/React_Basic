ba0bd2fac1ee9a81618f28e078c3be6908032b8d
INSHAKE <inseok5053@gmail.com>
2023-07-09 21:38:13 +0900

    리액트 시작

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // 서버에서 변수 가져왔다 치고, 여긴 서버에서 받는 정보임@@@ 단순 선언 아님@@@
+  let mainTitle = "INSHAKE";
+  let [postTitle] = useState(["글제목1", "글제목2", "글제목3"]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 상단 메뉴 */}
+      <div className="nav-bar">
+        <h4>INSHAKE'S BLOG</h4>
+      </div>
+      <div>
+        <h1>{ mainTitle }</h1>
+      </div>
+
+      {/* 발행 글 목록 UI */}
+      <div className="post-list">
+        <h4>{postTitle[0]}</h4>
+        <p>발행날짜</p>
+      </div>
+      <div className="post-list">
+        <h4>{postTitle[1]}</h4>
+        <p>발행날짜</p>
+      </div>
+      <div className="post-list">
+        <h4>{postTitle[2]}</h4>
+        <p>발행날짜</p>
+      </div>
     </div>
+
+
   );
 }
 
